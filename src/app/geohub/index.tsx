90e8e6fd5f530c08adb57e67e35675a9e4c17bc0
Dom-iha <taqib.ibrahim@gmail.com>
2024-01-31 22:01:36 +0100

    Initial commit

---
+++ src/app/geohub/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Geohub() {
+  return (
+    <div>
+      <h1>GeoHub</h1>
+    </div>
+  )
+}
+
+export default Geohub
---
+++ src/app/services/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Services: React.FC = () => {
+   return (
+      <div>
+         <h1>Services Page</h1>
+         <p>Welcome to our services page!</p>
+      </div>
+   );
+};
+
+export default Services;
