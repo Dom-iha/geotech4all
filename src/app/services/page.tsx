9df785bcc6b1dd18aa3871c8577582aa94c4c201
Dom-iha <taqib.ibrahim@gmail.com>
2024-04-14 22:37:42 +0100

    ui updates

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,6 +3,8 @@ import geohub from '../../../public/assets/icons/geohub.jpg';
 import Link from 'next/link';
 import { Metadata } from 'next';
 import { DoubleArrowDownIcon, ArrowRightIcon } from '@radix-ui/react-icons';
+import data from '@/data/data.json';
+import Product from '@/components/cards/product';
 
 export const metadata: Metadata = {
   title: 'Services',
@@ -16,11 +18,11 @@ const Services: React.FC = () => {
             Tailor-made solutions for your geoscience needs
           </h1>
           <p className='text-center mt-4 text-xl lg:text-2xl max-w-[900px]'>
-            From large enterprises to academia – we help the community solve geo
+            From enterprises to academia – we help the community solve geo
             problems.
           </p>
           <Link
-            href='#showcase'
+            href='#services'
             className='w-[60px] h-[60px] lg:h-[100px] lg:w-[100px]  grid place-items-center border-accent border-dashed border rounded-full'
           >
             <DoubleArrowDownIcon className='down-arrow' />
@@ -57,7 +59,7 @@ const Services: React.FC = () => {
               papers.
             </p>
             <Link
-              href={`/services/geological-services`}
+              href={`/services/project-writing`}
               className='w-fit p-2 flex items-center justify-center font-semibold rounded-md gap-2 bg-accent text-main min-w-[8rem] hover:gap-4 focus-visible:gap-4 focus-visible:outline-accent outline-offset-1 outline-1 focus-visible:outline-dashed transition-all duration-300'
             >
               Details
@@ -72,7 +74,7 @@ const Services: React.FC = () => {
               surveys.
             </p>
             <Link
-              href={`/services/geological-services`}
+              href={`/services/field-mapping`}
               className='w-fit p-2 flex items-center justify-center font-semibold rounded-md gap-2 bg-accent text-main min-w-[8rem] hover:gap-4 focus-visible:gap-4 focus-visible:outline-accent outline-offset-1 outline-1 focus-visible:outline-dashed transition-all duration-300'
             >
               Details
@@ -88,7 +90,7 @@ const Services: React.FC = () => {
               popular geological software incliding ArcGis, Petrel and Surfer.
             </p>
             <Link
-              href={`/services/geological-services`}
+              href={`/services/software-training`}
               className='w-fit p-2 flex items-center justify-center font-semibold rounded-md gap-2 bg-accent text-main min-w-[8rem] hover:gap-4 focus-visible:gap-4 focus-visible:outline-accent outline-offset-1 outline-1 focus-visible:outline-dashed transition-all duration-300'
             >
               Details
@@ -105,7 +107,7 @@ const Services: React.FC = () => {
               activities.
             </p>
             <Link
-              href={`/services/geological-services`}
+              href={`/services/equipment-sales`}
               className='w-fit p-2 flex items-center justify-center font-semibold rounded-md gap-2 bg-accent text-main min-w-[8rem] hover:gap-4 focus-visible:gap-4 focus-visible:outline-accent outline-offset-1 outline-1 focus-visible:outline-dashed transition-all duration-300'
             >
               Details
@@ -121,7 +123,7 @@ const Services: React.FC = () => {
               to conduct non-invasive subsurface investigations
             </p>
             <Link
-              href={`/services/geological-services`}
+              href={`/services/geophysical-survey`}
               className='w-fit p-2 flex items-center justify-center font-semibold rounded-md gap-2 bg-accent text-main min-w-[8rem] hover:gap-4 focus-visible:gap-4 focus-visible:outline-accent outline-offset-1 outline-1 focus-visible:outline-dashed transition-all duration-300'
             >
               Details
@@ -132,62 +134,16 @@ const Services: React.FC = () => {
       </section>
       <section className='px-6 md:px-8 lg:px-24 py-14'>
         <h2 className='text-2xl lg:text-4xl font-bold mb-16'>Products</h2>
-        <ul className='grid gap-8 lg:gap-32 mt-5 grid-cols-[repeat(auto-fit,_minmax(16rem,_1fr))]'>
-          <li className='grid gap-4 lg:gap-8'>
-            <h3 className='text-xl lg:text-2xl font-semibold'>
-              Geological Services
-            </h3>
-            <p className='lg:text-xl'>
-              Our geological services encompass a wide range of expertise
-              including geological mapping, mineral exploration, and site
-              characterization.
-            </p>
-            <button
-              type='button'
-              className='cursor-pointer relative group overflow-hidden border-2 px-10 py-2 border-accent w-fit rounded-md'
-            >
-              <span className='uppercase font-medium text-main lg:text-lg relative z-10 group-hover:text-accent duration-500'>
-                Details
-              </span>
-              <span className='absolute top-0 left-0 w-full h-full bg-accent origin-left group-hover:origin-right group-hover:scale-x-0 transition duration-300'></span>
-            </button>
-          </li>
-          <li className='grid gap-4 lg:gap-8'>
-            <h3 className='text-xl lg:text-2xl font-semibold'>
-              Research Assistance
-            </h3>
-            <p className='lg:text-xl'>
-              Our project writing services offer professional assistance in
-              preparing high-quality project proposals, reports and research
-              papers.
-            </p>
-            <button
-              type='button'
-              className='cursor-pointer relative group overflow-hidden border-2 px-10 py-2 border-accent w-fit rounded-md'
-            >
-              <span className='uppercase font-medium text-main lg:text-lg relative z-10 group-hover:text-accent duration-500'>
-                Details
-              </span>
-              <span className='absolute top-0 left-0 w-full h-full bg-accent origin-left group-hover:origin-right group-hover:scale-x-0 transition duration-300'></span>
-            </button>
-          </li>
-          <li className='grid gap-4 lg:gap-8'>
-            <h3 className='text-xl lg:text-2xl font-semibold'>Field Mapping</h3>
-            <p className='lg:text-xl'>
-              Our field mapping services combine fieldwork expertise with
-              advanced mapping technologies to facilitate accurate geological
-              surveys.
-            </p>
-            <button
-              type='button'
-              className='cursor-pointer relative group overflow-hidden border-2 px-10 py-2 border-accent w-fit rounded-md'
-            >
-              <span className='uppercase font-medium text-main lg:text-lg relative z-10 group-hover:text-accent duration-500'>
-                Details
-              </span>
-              <span className='absolute top-0 left-0 w-full h-full bg-accent origin-left group-hover:origin-right group-hover:scale-x-0 transition duration-300'></span>
-            </button>
-          </li>
+        <ul className='grid gap-8 lg:gap-10 mt-5 grid-cols-[repeat(auto-fit,_minmax(16rem,_1fr))]'>
+          {data.products.slice(0, 3).map((product) => (
+            <Product
+              key={product.name}
+              name={product.name}
+              cover={product.cover}
+              description={product.description}
+              availability={product.availability}
+            />
+          ))}
         </ul>
       </section>
       <section className='px-6 md:px-8 lg:px-24 py-14'>
