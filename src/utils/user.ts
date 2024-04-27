ea50e224e9e24d1830ca2a36ad74446e3cd8d60d
Dom-iha <taqib.ibrahim@gmail.com>
2024-04-27 21:55:42 +0100

    fix issue with dynamic path for (auth) route

--- src/utils/user.ts
+++
@@ -1,7 +0,0 @@
-import { auth, currentUser } from '@clerk/nextjs';
-
-export const getUser = async () => {
-  const { userId } = auth();
-  const user = await currentUser();
-  return { userId, user };
-};
